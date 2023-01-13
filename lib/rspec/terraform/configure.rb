fad322c3c66f98b2c60623dcc7305e276916c24a
plukevdh <hi@plukevdh.me>
2023-01-13 15:25:55 -0500

    bad var name
    copy-paste problems from original project

--- lib/rspec/terraform/configure.rb
+++ lib/rspec/terraform/configure.rb
@@ -1,9 +1,9 @@
 require_relative 'plan_helpers'
 
-RSpec.configure do |c|
+RSpec.configure do |config|
   config.add_setting :terraform_binary, default: "/Users/`whoami`/.asdf/shims/terraform"
   config.add_setting :terraform_examples_dir, default: "examples"
   config.add_setting :silence_terraform_output, default: false
 
-  c.include(RSpec::Terraform::PlanHelpers)
+  config.include(RSpec::Terraform::PlanHelpers)
 end
