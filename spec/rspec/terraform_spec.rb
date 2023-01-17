af82aedb99c97d4bd04ad0922da8a91a3166d8f8
plukevdh <hi@plukevdh.me>
2023-01-17 09:56:08 -0500

    fix failing spec

--- spec/rspec/terraform_spec.rb
+++ spec/rspec/terraform_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'spec_helper'
 
-RSpec.describe Rspec::Terraform do
+RSpec.describe RSpec::Terraform do
   it "has a version number" do
     expect(Rspec::Terraform::VERSION).not_to be nil
   end
